e1aed17eb24e0e4ba15a6e62b8c0da6d573b4cbe
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-15 14:12:59 -0600

    Add test for finding unique items in all food truck inventories

--- test/event_test.rb
+++ test/event_test.rb
@@ -65,6 +65,20 @@ class EventTest < Minitest::Test
     assert_equal 243.75, @food_truck3.potential_revenue
   end
 
+  def test_it_can_find_unique_items
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+    @food_truck3.stock(@item3, 10)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    assert_equal [@item1, @item2, @item4, @item3], @event.unique_items
+  end
+
   def test_it_can_get_total_inventory
     @food_truck1.stock(@item1, 35)
     @food_truck1.stock(@item2, 7)
