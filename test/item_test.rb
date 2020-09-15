28562edfdd4d1e4bbf4eb5a4b4ebe726ac52e1d2
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-15 13:12:45 -0600

    add item attributes test

--- test/item_test.rb
+++ test/item_test.rb
@@ -3,12 +3,20 @@ require "minitest/pride"
 require "./lib/item"
 
 class ItemTest < Minitest::Test
+  def setup
+    @item1 = Item.new({name: "Peach Pie (Slice)", price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
 
   def test_it_is_item
-    item1 = Item.new({name: "Peach Pie (Slice)", price: "$3.75"})
-    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    assert_instance_of Item, @item1
+    assert_instance_of Item, @item2
+  end
 
-    assert_instance_of Item, item1
-    assert_instance_of Item, item2
+  def test_it_has_attributes
+    assert_equal "Peach Pie (Slice)", @item1.name
+    assert_equal "Apple Pie (Slcie)", @item2.name
+    assert_equal 3.75, @item1.price
+    assert_equal 2.50, @item2.price
   end
 end
