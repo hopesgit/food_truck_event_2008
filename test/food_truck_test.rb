ee7552abc8e604866b3f9a9920ef050d871fa8ed
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-15 13:19:30 -0600

    Add attribute assertions to foodtruck test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -3,9 +3,16 @@ require "minitest/pride"
 require "./lib/food_truck"
 
 class FoodTruckTest < Minitest::Test
-  def test_it_is_food_truck
-    food_truck = FoodTruck.new("Rocky Mountain Pies")
+  def setup
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
 
-    assert_instance_of FoodTruck, food_truck
+  def test_it_is_food_truck
+    assert_instance_of FoodTruck, @food_truck
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+    assert_equal [], @food_truck.inventory
   end
 end
