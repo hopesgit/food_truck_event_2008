07ba57d2c7c88a540b20ff261f6a91401e120290
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-15 13:44:32 -0600

    Add attribute tests for Event

--- test/event_test.rb
+++ test/event_test.rb
@@ -18,4 +18,9 @@ class EventTest < Minitest::Test
   def test_it_is_an_event
     assert_instance_of Event, @event
   end
+
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @event.name
+    assert_instance_of Array, @event.food_trucks
+  end
 end
